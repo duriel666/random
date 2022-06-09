09cbdbe04cbcfb9c0a900ab47a209048747b0e85
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-06-09 12:35:06 +0300

    edit

--- random-python/list-stuff.py
+++ random-python/list-stuff.py
@@ -1,8 +1,12 @@
 a = [1, 2, 3, 4, 5, 2, 3, 4, 3]  # list
 b = set(())  # set
+c = []
+d = a
 for i in a:
     b.add(i)  # set takes no duplicates
+    c.append(i)
 print(a)
 print(b)
 a.sort()
 print(a)
+print(id(a), id(b), id(c), id(d))
