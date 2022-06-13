d288b3b4391b073aca61c3769883fa0f21d78e17
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-06-13 20:22:11 +0300

    mo hr

---
+++ random-python/hr-collectiosordereddict.py
@@ -0,0 +1,12 @@
+n = int(input())
+lista = {}
+for i in range(n):
+    x = input().split(" ")
+    hinta=int(x[-1])
+    tavara=" ".join(x[:-1])
+    if tavara in lista:
+        lista[tavara] += hinta
+    else:
+        lista[tavara] = hinta
+for x,y in lista.items():
+    print(x,y)
