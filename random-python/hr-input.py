c7de742aabbaa869ace3bb80768ca7130ed777b8
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-06-14 10:39:07 +0300

    edit

--- random-python/hr-input.py
+++ random-python/hr-input.py
@@ -4,14 +4,14 @@
 #x, k = map(int, input().split(" "))
 #print(eval(input()) == k)
 
-'''x, result = map(int, ("1 4").split(" "))
+x, result = map(int, ("1 4").split(" "))
 print(eval("x**3 + x**2 + x + 1") == result)
 
 x = int(input("Anna x:n arvo: "))
 lasku = input("Anna laskutehtävä: ")
 print(eval(lasku))
 
-eval(input())'''
+eval(input())
 
 x = int(input("Anna x: "))
 print("Tulos: "+str(eval(input("Anna lasku: "))))
---
+++ random-python/print-testailua.py
@@ -0,0 +1,5 @@
+n = int(input("numero: "))
+print("testi" if n < 3 else "kakka")
+
+x = int(input("Anna x: "))
+print("Tulos: "+str(eval(input("Anna lasku: "))))
