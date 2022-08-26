3e72a19bc9db9117de6230d24c4f379935d60b0d
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-08-26 12:12:39 +0300

    edit

--- random-python/python-5-21-kirjanruudukko.py
+++ random-python/python-5-21-kirjanruudukko.py
@@ -1,11 +1,12 @@
 # Kirjoita ratkaisu tähän
 x = int(input("Kerrokset: "))
 merkit = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for i in range(x-1, -1, -1):  # rivi 1 vain yhtä merkkiä, rivi x alkuun x merkkiä alaspäin alkumerkistä
+# rivi 1 vain yhtä merkkiä, rivi x alkuun x merkkiä alaspäin alkumerkistä
+for i in range(x-1, -1, -1):
     row = ""
     for j in range(x-1, 0+i, -1):
         row += merkit[j]
-    row+=merkit[i]*i*2
+    row += merkit[i]*i*2
     for j in range(i, x):  # kolmas looppi mikä lisää nuo alun ja lopun kirjaimet stringiin
         row += merkit[j]
     print(row)
@@ -13,7 +14,7 @@ for i in range(1, x):
     row = ""
     for j in range(x-1, 0+i, -1):
         row += merkit[j]
-    row+=merkit[i]*i*2
+    row += merkit[i]*i*2
     for j in range(i, x):
         row += merkit[j]
     print(row)
