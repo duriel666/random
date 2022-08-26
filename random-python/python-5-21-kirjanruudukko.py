e4f7fbf6b33118df342f6eacd163851493ca8f06
duriel666 <59049578+duriel666@users.noreply.github.com>
2022-08-26 12:23:32 +0300

    edit

--- random-python/python-5-21-kirjanruudukko.py
+++ random-python/python-5-21-kirjanruudukko.py
@@ -17,3 +17,26 @@ for i in range(1, x):
     for j in range(i, x):
         row += merkit[j]
     print(row)
+
+# esimerkkivastaus
+
+'''n = int(input("Kerrokset: "))
+
+aakkoset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+vasen = ""    # alaspäin menevä osa
+oikea = ""    # ylöspäin menevä osa
+k = n-1       # seuraavan merkin kohta aakkosissa
+m = 2*n-1     # väliin tulevien merkkien määrä
+while k >= 1:
+    vasen = vasen+aakkoset[k]
+    oikea = aakkoset[k]+oikea
+    m -= 2
+    print(vasen+aakkoset[k]*m+oikea)
+    k -= 1
+while k <= n-1:
+    print(vasen+aakkoset[k]*m+oikea)
+    vasen = vasen[:-1]
+    oikea = oikea[1:]
+    m += 2
+    k += 1'''
